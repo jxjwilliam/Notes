3e6466ccf4b551441edf4be2374f58f995e721de
William Jiang <william@overpass.com>
2017-02-10 16:31:58 -0500

    update

--- overpass/query/q1.sql
+++ overpass/query/q1.sql
@@ -36,35 +36,6 @@ where any p in uc.permissions satisfies p.environmentPk = 'environment::45a9fbf2
 select * from app where type = 'user-credentials' limit 1
 
 
-SELECT 
-    u.fullName.\`first` AS \`first\`, 
-    u.fullName.\`last` AS \`last\`, 
-    uc.email
-    FROM app uc USE KEYS 'user-credentials::wiliam@overpass.com'
-    LEFT JOIN app u ON KEYS uc.userPk
-    LEFT JOIN state s ON KEYS "user-state::" || meta(u).id || "::environment::45a9fbf2-b565-4922-ad58-980aa677deae"
-
- SELECT uc.*,
-    u.fullName.\`first` AS \`first\`, 
-    u.fullName.\`last` AS \`last\`, 
-    uc.email
-    FROM app uc USE KEYS 'user-credentials::wiliam@overpass.com'
-    LEFT JOIN app u ON KEYS uc.userPk
-    LEFT JOIN state s ON KEYS "user-state::" || meta(u).id || "::environment::45a9fbf2-b565-4922-ad58-980aa677deae"
-    
-SELECT 
-    u.fullName.\`first` AS \`first\`, 
-    u.fullName.\`last` AS \`last\`, 
-    u.email,
-    s.*
-    FROM app u USE KEYS [
-        'user::0afb03a8-58d8-48a4-ac19-6f68b1f36992',
-        "user::56354979-7de5-4318-bdc0-c7f310c618a3", 
-        "user::bac2271b-5002-4863-b36e-6edfffac9ab2", 
-        "user::f639e918-9917-484b-b932-3290138c4c24"
-    ]
-    LEFT JOIN state s ON KEYS "user-state::" || meta(u).id || "::environment::45a9fbf2-b565-4922-ad58-980aa677deae"
-
   
 SELECT
 uc.userPk AS userId,
@@ -127,3 +98,56 @@ SELECT
     AND ai.campaignId = 'campaign::a59c6fdf-9c37-4864-b98d-6baf7466a231'
     AND ai.environmentId = 'environment::8ebc97a7-f1d6-4b2d-a2b7-adac3d0b483a'
     AND ANY participant IN ai.participants SATISFIES participant.id = 'user::ed8d37c6-b7c7-4bb1-a808-f684924fa51b' END
+
+
+ SELECT
+uc.userPk AS userId,
+u.fullName.`first` || ' ' || u.fullName.`last` AS fullName,
+IFMISSING(u.email, uc.email) AS email,
+u.isActive,
+IFMISSING(s.lastKnownStatus, 'UNAVAILABLE') AS status,
+s.currentLoginSession AS currentSession,
+s.loginSessions AS loginSessions,
+(
+    SELECT
+    ARRAY_SORT(ARRAY_REMOVE(ARRAY_AGG(ai.disposition), "")) AS outcomes,
+    count(*) AS calls
+    FROM app ai
+    WHERE ai.type = 'agent-interaction'
+    AND ai.campaignId = 'campaign::0a6be69a-470c-4a8a-ae68-623592486d5b'
+    AND ai.environmentId = 'environment::45a9fbf2-b565-4922-ad58-980aa677deae'
+    AND ANY participant IN ai.participants SATISFIES participant.id = uc.userPk END
+) AS interactions
+FROM app uc 
+LEFT JOIN app u ON KEYS uc.userPk
+LEFT JOIN state s ON KEYS 'user-state::' || uc.userPk || '::environment::45a9fbf2-b565-4922-ad58-980aa677deae'
+WHERE uc .type = 'user-credentials'
+AND ANY x IN uc.permissions SATISFIES 
+    x.levelPk = 'campaign::0a6be69a-470c-4a8a-ae68-623592486d5b'  
+    AND x.environmentPk = 'environment::45a9fbf2-b565-4922-ad58-980aa677deae'
+    AND x.`role` = 'agent'
+END;   
+
+
+export stage="testing"
+
+
+(1)
+Repository    Branch
+chaim       campaign-monitor-ui
+olam        campaign-monitor-ui
+gps         development
+
+
+(2) Login with:
+login user: bpop@mailinator.com 
+login pass: Benjamin001
+
+
+(3) location:
+campaign -> Monitor -> Agents
+
+
+(4) The design is:
+https://projects.invisionapp.com/share/JF7W69FK4#/screens/178441541
+https://slack-files.com/files-pri-safe/T0L367NHM-F3ZL47R44/campaign_monitor_v2.pdf?c=1485982213-baf49e4886b5b5e39d652ec63c628c98314a0a99
